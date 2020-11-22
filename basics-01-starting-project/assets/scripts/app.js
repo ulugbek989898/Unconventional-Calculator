50a94fba0970d132993ba220a6829ce87798493d
ulugbek989898 <47236469+ulugbek989898@users.noreply.github.com>
2020-11-22 17:09:34 +0500

    Add files via upload
    final solution Unconventional Calculator

---
+++ basics-01-starting-project/assets/scripts/app.js
@@ -0,0 +1,83 @@
+const defaultResult = 0;
+let currentResult = defaultResult;
+let logEntries = [];
+
+// Gets input from input field
+function getUserNumberInput() {
+    return parseInt(userInput.value);
+}
+
+// Generates and writes calculation log
+function createAndWriteOutput(operator, resultBeforeCalc, calcNum) {
+    const calcDescription = `${resultBeforeCalc} ${operator} ${calcNum}`;
+    outputResult(currentResult, calcDescription); //from vender.js file
+}
+
+function writeToLog(
+    operationIdentifier,
+    prevResult,
+    operationNumber,
+    newResult
+) {
+    const logEntry = {
+        operation: operationIdentifier,
+        prevResult: prevResult,
+        number: operationNumber,
+        result: newResult,
+    };
+    logEntries.push(logEntry);
+    console.log(logEntries);
+}
+function calculateResult(calculationType) {
+    if (
+        calculationType !== "ADD" &&
+        calculationType !== "SUBTRACT" &&
+        calculationType !== "MULTIPLY" &&
+        calculationType !== "DIVIDE"
+    ) {
+        return;
+    }
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    let mathOperator;
+    if (calculationType === "ADD") {
+        currentResult += enteredNumber;
+        mathOperator = "+";
+    } else if (calculationType === "SUBTRACT") {
+        currentResult -= enteredNumber;
+        mathOperator = "-";
+    } else if (calculationType === "MULTIPLY") {
+        currentResult *= enteredNumber;
+        mathOperator = "*";
+    } else if (calculationType === "DIVIDE") {
+        currentResult /= enteredNumber;
+        mathOperator = "/";
+    }
+
+    createAndWriteOutput(mathOperator, initialResult, enteredNumber);
+    writeToLog(calculationType, initialResult, enteredNumber, currentResult);
+}
+
+function add() {
+    calculateResult("ADD");
+}
+
+function subtract() {
+    calculateResult("SUBTRACT");
+}
+
+function multiply() {
+    calculateResult("MULTIPLY");
+}
+
+function divide() {
+    calculateResult("DIVIDE");
+}
+
+addBtn.addEventListener("click", add);
+
+subtractBtn.addEventListener("click", subtract);
+
+multiplyBtn.addEventListener("click", multiply);
+
+divideBtn.addEventListener("click", divide);
